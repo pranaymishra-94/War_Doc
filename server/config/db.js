e7d552049bdce6603ed15bfc294f5e691c4c47d5
pranaymishra-94 <pranayraj930@gmail.com>
2025-08-12 14:42:08 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,8 @@
-// server/config/db.js
 
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    // The options object is no longer needed
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: 🌿`);
